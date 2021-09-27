04256a14a8e8208d5c0c74166f5007068e04e510
Cain Hall <cain.hall98@gmail.com>
2021-09-27 16:29:09 +1000

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const chromium = require('chrome-aws-lambda');
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+export default async (req, res) => {
+
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto('https://example.com');
+
+    result = await page.title();
+    res.statusCode = 200
+    return res.json({ result })  
+  } catch (error) {
+    // return callback(error);
+    res.statusCode = 200
+    return res.json(error)  
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
 }
